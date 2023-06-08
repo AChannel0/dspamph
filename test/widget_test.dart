45eafd1f11f61ea5330ec393027362045f3c0a58
Fritz <fritzaeron.rose@wvsu.edu.ph>
2023-06-08 13:59:00 +0800

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:dspamph/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(DSpamPhApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
